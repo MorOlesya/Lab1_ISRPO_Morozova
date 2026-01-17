0546c1e61ea16f77497b5c5574e94db431e9d463
MorOlesya <dl9morozova@gmail.com>
2026-01-17 21:55:10 +0300

    Ввод имени, вывод дня недели и логика продвижения

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,5 +3,21 @@
 // Console.WriteLine("Морозова Олеся Сергеевна");
 // Console.WriteLine(DateTime.Now);
 
-
-
+System.Console.Write("Введите ваше имя: ");
+string? name = Console.ReadLine();
+System.Console.WriteLine($"Привет, {name}!");
+System.Console.WriteLine(DateTime.Now.DayOfWeek.ToString());
+System.Console.Write("Хотите продолжить? (y/n) ");
+string? answer = Console.ReadLine();
+if (answer == "y" || answer == "Y")
+{
+    System.Console.WriteLine("Продолжаем...");
+}
+else if (answer == "n" || answer == "N")
+{
+    System.Console.WriteLine("Остановка");
+}
+else
+{
+    System.Console.WriteLine("Попробуйте снова");
+}
